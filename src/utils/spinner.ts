d90222e4033b9183da133e0dfed304aefd0e6f1e
unix <unix.bio@gmail.com>
2022-02-06 06:00:35 +0800

    chore: upgrade func to solve syntax error

--- src/utils/spinner.ts
+++ src/utils/spinner.ts
@@ -1,41 +1,41 @@
-import ora, { Ora } from 'ora'
-import chalk from 'chalk'
-import ansiEscapes from 'ansi-escapes'
+import ora, { Ora } from "ora";
+import chalk from "chalk";
+import ansiEscapes from "ansi-escapes";
 
-type G = NodeJS.Global & {
-  spinner: Ora,
-}
-declare let global: G
+type G = Global & {
+  spinner: Ora;
+};
+declare let global: G;
 
 export const start = (text: string) => {
-  text = chalk.gray(text)
+  text = chalk.gray(text);
   if (!global.spinner) {
-    global.spinner = ora(text).start()
-    global.spinner.color = 'gray'
+    global.spinner = ora(text).start();
+    global.spinner.color = "gray";
   } else {
-    global.spinner.start(text)
+    global.spinner.start(text);
   }
-}
+};
 
 export const succeed = (clear: boolean = false) => {
-  if (!global.spinner) return
+  if (!global.spinner) return;
   if (clear) {
-    global.spinner.stop()
-    global.spinner.clear()
-    process.stderr.write(ansiEscapes.eraseLines(1))
+    global.spinner.stop();
+    global.spinner.clear();
+    process.stderr.write(ansiEscapes.eraseLines(1));
   } else {
-    global.spinner.succeed()
+    global.spinner.succeed();
   }
-  global.spinner = null
-}
+  global.spinner = null;
+};
 
 export const fail = (text?: string) => {
   if (global.spinner) {
-    global.spinner.fail()
-    console.log('')
+    global.spinner.fail();
+    console.log("");
   }
   if (text) {
-    console.log(`Error: ${text}`)
-    process.exit(1)
+    console.log(`Error: ${text}`);
+    process.exit(1);
   }
-}
+};
